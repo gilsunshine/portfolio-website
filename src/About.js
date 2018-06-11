4c318fabe8e41a4d79ac7607ae05b062daeb5f97
gilsunshine <gilsunshine@gmail.com>
2018-06-11 13:24:27 -0400

    about

--- src/About.js
+++ src/About.js
@@ -9,6 +9,7 @@ class About extends Component {
         <div className="AboutText">
           <p>Gil Sunshine is an artist and designer working at the intersection of art and architecture. He is currently pursuing a Masters of Architecture at MIT.</p>
           <a href="GIL_SUNSHINE_RESUME_2018_06_11.pdf" className="DownloadLink" download>Click here to download resume.</a>
+          <p>Website design and programming by Gil Sunshine.</p>
         </div>
         <br/>
         <a href="mailto:info@gilsunshine.com" class="fa fa-envelope"></a>
