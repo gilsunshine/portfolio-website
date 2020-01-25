6cadb04575e5a6d35e8750c630c97ffb474c8c77
Gil Sunshine <gils@Gils-MacBook-Pro.local>
2020-01-25 10:11:46 -0500

    redirect

--- src/Header.js
+++ src/Header.js
@@ -9,8 +9,8 @@ class Header extends Component {
           <p className="MyName" onClick={this.props.turnOffWork}>Gil Sunshine</p>
         </div>
         <div className="HeaderItems">
-          <p className="HeaderItem" onClick={this.props.toggleHardWork}>Hard Work</p>
-          <p className="HeaderItem" onClick={this.props.toggleSoftWork}>Soft Work</p>
+          <p className="HardWork" onClick={this.props.toggleHardWork}>Hard Work</p>
+          <p className="SoftWork" onClick={this.props.toggleSoftWork}>Soft Work</p>
           <p className="HeaderAbout" onClick={this.props.toggleAbout}>About</p>
         </div>
       </div>
