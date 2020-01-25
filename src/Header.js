1eaeb5283f137624caec78944477bacf3470eaf2
Gil Sunshine <gils@Gils-MacBook-Pro.local>
2020-01-25 12:09:20 -0500

    no projects

--- src/Header.js
+++ src/Header.js
@@ -9,8 +9,6 @@ class Header extends Component {
           <p className="MyName" onClick={this.props.turnOffWork}>Gil Sunshine</p>
         </div>
         <div className="HeaderItems">
-          <p className="HardWork" onClick={this.props.toggleHardWork}>Hard Work</p>
-          <p className="SoftWork" onClick={this.props.toggleSoftWork}>Soft Work</p>
           <p className="HeaderAbout" onClick={this.props.toggleAbout}>About</p>
         </div>
       </div>
