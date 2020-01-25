cddf20b4977dc1112709d4fd96ab39d2f079d28b
Gil Sunshine <gils@Gils-MacBook-Pro.local>
2020-01-25 08:12:11 -0500

    hard and soft work distinction

--- src/HardWork.js
+++ src/HardWork.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Sculpture27 from './projects/Sculpture27.js'
-import GestureDraw from './projects/GestureDraw.js'
+// import GestureDraw from './projects/GestureDraw.js'
 import NordstromSS16 from './projects/NordstromSS16.js'
 import Greenwich125 from './projects/Greenwich125.js'
 import SlipChair from './projects/SlipChair.js'
@@ -18,8 +18,8 @@ class HardWork extends Component {
         <div className="ProjectContainer">
           <Sculpture27 s27Slide={this.props.s27Slide} currentHardWork={this.props.currentHardWork} activeProject={this.props.activeProject} activateProject={this.props.activateProject}/>
           <br/><br/>
-          <GestureDraw gestureDrawSlide={this.props.s27Slide} currentHardWork={this.props.currentHardWork} activeProject={this.props.activeProject} activateProject={this.props.activateProject}/>
-          <br/><br/>
+          // <GestureDraw gestureDrawSlide={this.props.s27Slide} currentHardWork={this.props.currentHardWork} activeProject={this.props.activeProject} activateProject={this.props.activateProject}/>
+          // <br/><br/>
           <NordstromSS16 nordstromss16Slide={this.props.nordstromss16Slide} currentHardWork={this.props.currentHardWork} activeProject={this.props.activeProject} activateProject={this.props.activateProject}/>
           <br/><br/>
           <Greenwich125 greenwich125Slide={this.props.greenwich125Slide} currentHardWork={this.props.currentHardWork} activeProject={this.props.activeProject} activateProject={this.props.activateProject}/><br/><br/>
--- src/SoftWork.js
+++ src/SoftWork.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './SoftWork.css'
+import GestureDraw from './projects/GestureDraw.js'
 
 class SoftWork extends Component {
   render() {
     return (
       <div className="SoftWork">
-
+        <GestureDraw gestureDrawSlide={this.props.s27Slide} currentHardWork={this.props.currentHardWork} activeProject={this.props.activeProject} activateProject={this.props.activateProject}/>
+        <br/><br/>
       </div>
     );
   }
