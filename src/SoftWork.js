1f59f9302dc3f018d8f8a1fee7c307bd1c938a9c
Gil Sunshine <gils@Gils-MacBook-Pro.local>
2020-01-25 08:29:16 -0500

    soft work

--- src/SoftWork.js
+++ src/SoftWork.js
@@ -3,11 +3,15 @@ import './SoftWork.css'
 import GestureDraw from './projects/GestureDraw.js'
 
 class SoftWork extends Component {
+
   render() {
+    console.log(this.props.activeProject + "softwork")
     return (
       <div className="SoftWork">
-        <GestureDraw gestureDrawSlide={this.props.s27Slide} currentHardWork={this.props.currentHardWork} activeProject={this.props.activeProject} activateProject={this.props.activateProject}/>
+        <div className="ProjectContainer">
+        <GestureDraw gestureDrawSlide={this.props.s27Slide} currentSoftWork={this.props.currentSoftWork} activeProject={this.props.activeProject} activateProject={this.props.activateProject}/>
         <br/><br/>
+        </div>
       </div>
     );
   }
